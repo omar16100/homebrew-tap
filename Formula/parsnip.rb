20d390b59f57d829cb3c8bac31bacb0bd34962da
Your Name <you@example.com>
2025-12-14 15:17:44 +0800

    Update SHA256 hashes for v0.1.0

--- Formula/parsnip.rb
+++ Formula/parsnip.rb
@@ -7,18 +7,18 @@ class Parsnip < Formula
   on_macos do
     on_arm do
       url "https://github.com/omar16100/parsnip/releases/download/v#{version}/parsnip-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64"
+      sha256 "8f5c5f9c6689613a60c9533f06467f32a4d5e39273a77bb296334d993fbf17ad"
     end
     on_intel do
       url "https://github.com/omar16100/parsnip/releases/download/v#{version}/parsnip-macos-amd64.tar.gz"
-      sha256 "PLACEHOLDER_AMD64"
+      sha256 "126ff9bed9efd78ab7d74864baaf84120516c617317afb36651803cd67c0d3a7"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/omar16100/parsnip/releases/download/v#{version}/parsnip-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX"
+      sha256 "7f81f41014fe63f9384fbff61f231a5e2f41cafb4d42e569cae2000819edffb2"
     end
   end
 
